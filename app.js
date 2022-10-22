73f9b343ca37ed32929016fcb7ea468212619b2b
AbhishekkM05 <abhishek.mishra.05@outlook.com>
2022-10-22 19:35:39 +0530

    add local storage for form input

--- app.js
+++ app.js
@@ -22,3 +22,26 @@ form.addEventListener('submit', (e) => {
 });
 
 //local storage 
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('msg');
+if (localStorage.getItem('ContactForm')) {
+  const data = JSON.parse(localStorage.getItem('ContactForm'));
+  document.getElementById('name').value = data.name;
+  document.getElementById('email').value = data.email;
+  document.getElementById('msg').value = data.message;
+}
+function addLocalStorage(data) {
+  const dict = JSON.parse(localStorage.getItem('ContactForm'));
+  const newdict = { ...dict, ...data };
+  localStorage.setItem('ContactForm', JSON.stringify({ ...newdict }));
+}
+name.onchange = (e) => {
+  addLocalStorage({ name: e.target.value });
+};
+email.onchange = (e) => {
+  addLocalStorage({ email: e.target.value });
+};
+message.onchange = (e) => {
+  addLocalStorage({ message: e.target.value });
+};
