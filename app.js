149a70e71f553d041f66b01f6255a3bead379d61
AbhishekkM05 <abhishek.mishra.05@outlook.com>
2022-10-22 17:23:18 +0530

    add form validation

---
+++ app.js
@@ -0,0 +1,29 @@
+//form-validation 
+const projectnBtn = document.querySelectorAll('#popbtn');
+projectnBtn.forEach((btn, ind) => {
+  btn.addEventListener('click', () => {
+    Popup(ind);
+  });
+});
+
+const form = document.querySelector('#form');
+const inputEmail = document.querySelector('#email');
+
+const validateEmail = (email) => {
+  if (email === email.toLowerCase()) return true;
+  return false;
+};
+
+form.addEventListener('submit', (e) => {
+  const invalidMessage = 'Email should be lowercase letters';
+  const messageDisplay = document.getElementById('message');
+  e.preventDefault();
+
+  if ((!validateEmail(inputEmail.value.trim()))) {
+    messageDisplay.innerHTML = invalidMessage;
+    messageDisplay.classList.add('message');
+  } else {
+    messageDisplay.innerHTML = '';
+    form.submit();
+  }
+});
